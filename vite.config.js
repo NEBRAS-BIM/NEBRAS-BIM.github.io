642cb7ee80f9b61580afec5b4fc0a15301b100e0
odai salahat <odai@nebras-bim.com>
2025-11-09 11:41:12 +0200

    Fix: update vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/nebras-bim.github.io/',
+  base: '/NEBRAS-BIM.github.io/',
 })
 
